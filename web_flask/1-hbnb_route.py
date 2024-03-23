408562c0477a9f715e9dbce2a462a3e706b2c856
GodwinCyber <godwinchuks032@gmail.com>
2024-03-23 22:34:57 +0000

    hello flash

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 """Starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
+/hbnb: display “HBNB”
 You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask
@@ -10,7 +11,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """Display string Hello HBNB"""
     return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display string HBNB"""
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
