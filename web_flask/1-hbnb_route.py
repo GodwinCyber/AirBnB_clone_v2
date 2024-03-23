5b87cd7583072ee69b1a0def4368b4ff3fa13e5d
GodwinCyber <godwinchuks032@gmail.com>
2024-03-23 22:45:16 +0000

    hello flash

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,18 +6,23 @@ Routes:
 /hbnb: display “HBNB”
 You must use the option strict_slashes=False in your route definition
 """
+
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Display string Hello HBNB"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Display string HBNB"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
