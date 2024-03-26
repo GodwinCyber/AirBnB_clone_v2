2066203c1c1acc8eb7011634b3f45dd5d708d821
GodwinCyber <godwinchuks032@gmail.com>
2024-03-25 17:40:56 -0700

    updated the storage files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,6 +63,5 @@ class FileStorage:
             del self.__objects[key]
 
     def close(self):
-	"""call reload() method for deserializing
-	the JSON file to objects"""
+        """call reload() method for deserializing the JSON file to objects"""
         self.reload()
